95d22371ccc4112e464d229f0f1a1e90f1f7af65
DeepSoupp <mmaba@naver.com>
2024-10-31 11:43:23 +0900

    feat: 유저 정보 조회 (#23)

--- src/components/mypage/MyProfile.jsx
+++ src/components/mypage/MyProfile.jsx
@@ -1,108 +1,180 @@
-import React, { useEffect, useState } from "react";
-import BookCard from "../ui/BookCard";
-import useUser from "../../hooks/useUser";
-import { getCookie } from "../../utils/cookieUtils";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import useUser from './useUser';
+import { getUserId } from './GetUserId';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { FaCaretDown } from 'react-icons/fa';
+import { getCookie } from '../../utils/cookieUtils';
+import axios from 'axios';
 
 const MyProfile = ({ userId }) => {
-  const { userData} = useUser(userId)
-  const [books, setBooks] = useState([]);
-  const [currentPage, setCurrentPage] = useState(0)
-  const [totalPages, setTotalPages] = useState(0);
-  const [totalBooksCount, setTotalBooksCount] = useState(0);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { userData, setUserData } = useUser(userId);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const logInUserId = getUserId();
+  const [currentPage, setCurrentPage] = useState('');
+  const [error, setError] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [username, setUsername] = useState('');
+  const [profileImage, setProfileImage] = useState(null);
 
   useEffect(() => {
-    const fetchBooks = async () => {
-      if (!userId) {
-        console.error("User Id 정의안됨")
-        return
-      }
-      try {
-        const token = getCookie("accessToken"); // 토큰 가져오기
-        const response = await axios.get(`${import.meta.env.VITE_REST_SERVER}/mypage/books/${userId}?page=${currentPage}`, {
-            headers: {
-                Authorization: `Bearer ${token}` // 헤더에 토큰 추가
-            }
-        })
-        setBooks(response.data.content) // 사용자 데이터 저장
-        setTotalPages(response.data.totalPages)
-        setTotalBooksCount(response.data.totalElements)
-      } catch (err) {
-        console.error(err)
-      }
+    if (userData) {
+      setUsername(userData.userName);
+      setMenuOpen(false);
     }
-    fetchBooks()
-  }, [userId, currentPage]);
+  }, [location, userData]);
 
-  if (!userData) return null; // userData 가 없으면 null 반환
-  
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber)
-  }
+  const handleMenuClick = (path, pageName) => {
+    setMenuOpen(false);
+    navigate(path);
+    setCurrentPage(pageName);
+  };
+
+  useEffect(() => {
+    const path = location.pathname;
+    if (path.includes('mypage')) {
+      setCurrentPage('마이페이지');
+    } else if (path.includes('mybooks')) {
+      setCurrentPage('문제집 관리');
+    } else if (path.includes('myquestions')) {
+      setCurrentPage('내가 푼 문제들');
+    } else if (path.includes('study')) {
+      setCurrentPage('스터디 관리');
+    }
+  }, [location]);
+
+  const handleEditClick = () => {
+    setIsEditing((prev) => !prev);
+  };
+
+  const handleSave = async () => {
+    try {
+      const token = getCookie('accessToken');
+      const formData = new FormData();
+      if (username) {
+        formData.append('newUserName', username);
+      }
+      if (profileImage) {
+        formData.append('file', profileImage);
+      }
+      await axios.patch(
+        `${import.meta.env.VITE_REST_SERVER}/mypage`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+
+      setUserData((prevData) => ({
+        ...prevData,
+        userName: username || prevData.userName,
+        profileImage: profileImage
+          ? URL.createObjectURL(profileImage)
+          : prevData.profileImage,
+      }));
+
+      setIsEditing(false);
+    } catch (err) {
+      setError(err);
+    }
+  };
+
+  if (!userData) return null;
+
   return (
-    <div>
-      <div className="flex items-center mb-8">
-        {userData.profileImage ? (
-          // 프로필 이미지가 있으면 표시
-          <img
-          src={userData.profileImage}
-          alt={`${userData.userName}'s profile`}
-          className="w-10 h-10 rounded-full mr-4"
-          />
-        ) : (
-          // 프로필 이미지가 없으면 기본 원형
-          <div className="w-10 h-10 rounded-full bg-black mr-4" />
-        )}
-        <div className="text-l font-semibold">{userData.userName}</div>
-      </div>
-      <div className="border-b border-gray-300 mb-8" />
+    <div className="relative">
+      <div className="flex items-center mb-8 justify-between">
+        <div className="flex items-center">
+          {userData.profileImage ? (
+            <img
+              src={userData.profileImage}
+              alt={`${userData.userName}'s profile`}
+              className="w-10 h-10 rounded-full mr-4"
+            />
+          ) : (
+            <div className="w-10 h-10 rounded-full bg-black mr-4" />
+          )}
+          <div className="text-l font-semibold">{userData.userName}</div>
+          {logInUserId === userId && (
+            <button className="ml-2 text-blue-500" onClick={handleEditClick}>
+              {isEditing ? '취소' : '수정'}
+            </button>
+          )}
+        </div>
 
-      <div className="flex justify-between items mb-4">
-        <div className="text-xl font-semibold mb-7">
-          {userData.userName} 님의 문제집 목록 [{totalBooksCount}]
+        <div className="relative mr-20 flex items-center">
+          <div
+            className="flex items-center border border-gray-300 rounded-md px-2 py-1 cursor-pointer"
+            onClick={() => setMenuOpen((prev) => !prev)}
+          >
+            <span className="mr-2">{currentPage}</span>
+            <FaCaretDown />
+          </div>
+          {menuOpen && (
+            <div className="absolute right-0 top-10 mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-50">
+              <ul className="py-1">
+                <li
+                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() =>
+                    handleMenuClick(`/mypage/${userId}`, '마이페이지')
+                  }
+                >
+                  마이페이지
+                </li>
+                <li
+                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() => handleMenuClick('/mybooks', '문제집 관리')}
+                >
+                  문제집 관리
+                </li>
+                <li
+                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() =>
+                    handleMenuClick('/myquestions', '내가 푼 문제들')
+                  }
+                >
+                  내가 푼 문제들
+                </li>
+                <li
+                  className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() => handleMenuClick('/mystudies', '스터디 관리')}
+                >
+                  스터디 관리
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {books.map((book) => (
-          // 책 목록을 BookCard 컴포넌트로 매핑
-          <div key={book.id} className="flex h-full">
-            <BookCard
-              title={book.title}
-              author={book.username}
-              bookmarkCount={book.bookmarkCount}
-              category={book.category}
-              />
-          </div>
-        ))}
-      </div>
-
-      {/* 페이지네이션 */}
-      <div className="flex justify-center mt-8 mb-10">
-        <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 0}>
-          <FaChevronLeft className="text-gray-500 text-sm" />
-        </button>
-        {Array.from({ length: totalPages }).map((_, index) => (
+      {isEditing && (
+        <div className="mb-4">
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="border border-gray-300 p-2 w-full mb-2"
+            placeholder="이름을 입력하세요"
+          />
+          <input
+            type="file"
+            onChange={(e) => setProfileImage(e.target.files[0])}
+            className="border border-gray-300 p-2 w-full mb-2"
+          />
           <button
-          key={index}
-            onClick={() => handlePageChange(index)}
-            className={`mx-1 ${
-              index + 1 === currentPage
-                ? "font-bold text-blue-400"
-                : "text-gray-500"
-            }`}
-            >
-            {index + 1}
+            onClick={handleSave}
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            저장
           </button>
-        ))}
-        <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages - 1}>
-          <FaChevronRight className="text-gray-500 text-sm" />
-        </button>
-      </div>
-
+        </div>
+      )}
     </div>
   );
-}
+};
 
-  export default MyProfile;
+export default MyProfile;
