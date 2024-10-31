19788cb57f01e35ee16c908af104a4a4944da019
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-31 12:01:18 +0900

    feat: index에 UserProvider 추가 (#28)

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { UserProvider } from "./context/UserProvider";
 
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
+
   <QueryClientProvider client={queryClient}>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </QueryClientProvider>
   // </React.StrictMode>
 );
