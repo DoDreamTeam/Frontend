658f8d57f05d441f1579f1926f7b8970f1e328ff
DeepSoupp <mmaba@naver.com>
2024-11-04 14:12:33 +0900

    feat: 나의 스터디 목록 페이지 기능 연동 (#38)

--- src/pages/MyQuestions.jsx
+++ src/pages/MyQuestions.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import MyProfileMe from '../components/mypage/MyProfileMe';
+import MyAnswers from '../components/mypage/MyAnswers';
 
 const MyQuestions = () => {
   return (
     <div className="flex flex-col items-center w-full">
       <div className="w-full max-w-screen-lg mb-6">
         <MyProfileMe />
+      </div>
+      <div className="w-full max-w-screen-lg mb-6 border-t border-gray-300 pt-5">
+        <MyAnswers />
       </div>
     </div>
   );
--- src/pages/MyStudies.jsx
+++ src/pages/MyStudies.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import MyProfileMe from '../components/mypage/MyProfileMe';
+import MyStudyCommentLike from '../components/mypage/MyStudyCommentLike';
+import MyStudiesList from '../components/mypage/MyStudiesList';
 
 const MyStudies = () => {
   return (
     <div className="flex flex-col items-center w-full">
       <div className="w-full max-w-screen-lg mb-6">
         <MyProfileMe />
+      </div>
+      <div className="w-full max-w-screen-lg mb-6 border-t border-gray-300 pt-5">
+        <MyStudiesList />
+      </div>
+      <div className="w-full max-w-screen-lg mb-6 border-t border-gray-300 pt-5">
+        <MyStudyCommentLike />
       </div>
     </div>
   );
