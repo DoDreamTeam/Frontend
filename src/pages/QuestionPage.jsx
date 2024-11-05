3b1809c9a7ca2655c16d0e5d273b84e2811d6a94
seoyeon-jung <jungluchia1204@gmail.com>
2024-11-05 15:26:46 +0900

    feat: 내 문제집에 추가 버튼 구현 (#51)

--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import SolveQuestionForm from "../components/questionEvaluate/SolveQuestionForm";
 import { useNavigate, useParams } from "react-router-dom";
 import api from "../api/api";
+import AddToMyBooks from "../components/questionEvaluate/AddToMyBooks";
 
 const QuestionPage = () => {
   const { id, questionId } = useParams();
@@ -41,7 +42,12 @@ const QuestionPage = () => {
         {bookTitle}
       </div>
 
-      <div className="text-3xl text-blue-600 font-bold mb-4 text-left">Q.</div>
+      <div className="flex justify-between">
+        <div className="text-3xl text-blue-600 font-bold mb-4 text-left">
+          Q.
+        </div>
+        <AddToMyBooks />
+      </div>
       {question && (
         <div className="mb-16">
           <div className="text-xl">{question}</div>
