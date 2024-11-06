8caa03bf57abc645093decc9029d70887cfa2104
codesche <codesche@gmail.com>
2024-11-06 17:18:07 +0900

    feat: 스터디 정보 불러오기 (#57)

---
+++ src/components/studypage/StudyInfo.jsx
@@ -0,0 +1,96 @@
+import { useNavigate } from "react-router-dom";
+import defaultProfile from "../../assets/default_profile.jpg";
+import { useUser } from "../../context/UserProvider";
+import useModal from "../../hooks/useModal";
+import useAlert from "../../hooks/useAlert";
+import { MdDelete, MdEdit } from "react-icons/md";
+
+const StudyInfo = ( { studyData } ) => {
+    const { userInfo } = useUser();
+    const navigate = useNavigate();
+    const { openModal, closeModal, Modal } = useModal();
+    const { showAlert, Alert } = useAlert();
+
+    const { title, username, description, profileImage, userId } = studyData;
+    console.log(studyData);
+
+    // 스터디 삭제
+    const handleDelete = async () => {
+        try {
+            const response = await api.delete(`/study/${studyData.id}`);
+            if (response.status === 204) {
+                navigate("/study");
+                closeModal();
+                showAlert("정상적으로 스터디가 삭제되었습니다.");
+            }
+        } catch (error) {
+            console.error("Delete Study Error: ", error);
+        }
+    }
+
+    return (
+        <div className="flex justify-between items-start w-full mb-16">
+            <div className="flex flex-col">
+                <div className="flex items-center mb-6">
+                    <div className="mr-2 text-3xl font-semibold">{title}</div>
+                    <div className="mr-4 text-3sm">{description}</div>
+                    {userInfo?.userName === username && (
+                        <div className="flex justify-center mt-2">
+                            <button className="mx-1">
+                                <MdEdit
+                                    className="hover:text-blue-400"
+                                    onClick={() => navigate(`/study/${studyData.id}/edit`)}
+                                />
+                            </button>
+                            <button className="mx-1">
+                                <MdDelete className="hover:text-blue-400" onClick={openModal} />
+                            </button>
+                        </div>
+                    )}
+                </div>
+                <div className="flex items-center">
+                    <img
+                        src={profileImage || defaultProfile}
+                        alt="Profile"
+                        className="w-8 h-8 rounded-full cursor-pointer border-2 border-gray-300 mr-2"
+                    />
+                    <div
+                        className="hover:underline cursor-pointer"
+                        onClick={() => navigate(`/mypage/${userId}`)}
+                    >
+                        {username}
+                    </div>
+                </div>
+            </div>
+
+            {/* 삭제 모달 */}
+            <Modal style="w-120 text-center">
+                <div className="text-2xl font-semibold m-6">
+                    정말 스터디를 삭제하시겠습니까?
+                </div>
+                <div className="text-xs text-gray-500">
+                    삭제하면 더 이상 이 스터디를 풀던 사람들이 이용할 수 없습니다.
+                </div>
+                <div className="flex justify-around mt-4 w-full">
+                    <button
+                        className="w-3/4 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 m-4"
+                        onClick={handleDelete}
+                    >
+                        삭제
+                    </button>
+                    <button
+                        className="w-3/4 bg-gray-200 text-black py-2 px-4 rounded hover:bg-gray-400 m-4"
+                        onClick={closeModal}
+                    >
+                        취소
+                    </button>
+                </div>
+            </Modal>
+
+            {/* 삭제 확인 alert */}
+            <Alert />
+        </div>
+    )
+}
+ 
+export default StudyInfo;
