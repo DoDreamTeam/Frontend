b0f4b91e6df63ac43ead265698b0caa009bd9900
seoyeon-jung <jungluchia1204@gmail.com>
2024-11-06 14:29:03 +0900

    fix: 로그인 여부 / 빈 댓글 조건 추가 (#56)
    - 비회원이 댓글 작성 시 로그인 요청 Modal이 나오도록 구현했습니다
    - 댓글을 작성하지 않고 등록 버튼을 누르면 Modal이 나오도록 구현했습니다.

--- src/components/bookpage/comment/BookCommentForm.jsx
+++ src/components/bookpage/comment/BookCommentForm.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import CommentForm from "../../ui/CommentForm";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import api from "../../../api/api";
+import useModal from "../../../hooks/useModal";
+import { IoMdWarning } from "react-icons/io";
 
 const BookCommentForm = ({ bookId }) => {
   const queryClient = useQueryClient();
+  const { openModal, Modal, closeModal, isModalOpen } = useModal();
+  const [comment, setComment] = useState("");
+  const [modalMessage, setModalMessage] = useState("");
 
   const mutation = useMutation({
     mutationFn: async (comment) => {
@@ -17,13 +22,29 @@ const BookCommentForm = ({ bookId }) => {
       // 댓글 작성 성공 후 쿼리 무효화
       queryClient.invalidateQueries(["comments", bookId]);
     },
-    onError: () => {
-      console.log("COMMENT ERROR!");
+    onError: (error) => {
+      if (error.response && error.response.status === 403) {
+        setModalMessage("댓글을 작성하려면 로그인해 주세요."); // 403 에러 메시지 설정
+        openModal();
+      } else {
+        console.log("COMMENT ERROR!", error);
+      }
     },
   });
 
   const handleCommentSubmit = (comment) => {
-    mutation.mutate(comment); // 댓글 내용을 인자로 전달
+    if (!comment || comment.trim() === "") {
+      setModalMessage("댓글을 작성해야 합니다.");
+      openModal();
+      return;
+    }
+    mutation.mutate(comment);
+    setComment("");
+  };
+
+  const handleCloseModal = () => {
+    closeModal();
+    setModalMessage("");
   };
 
   return (
@@ -32,7 +53,27 @@ const BookCommentForm = ({ bookId }) => {
         placeholder="문제집에 궁금한 점이 있다면 댓글을 남겨주세요 :)"
         buttonText="작성"
         onSubmit={handleCommentSubmit}
+        comment={comment}
+        setComment={setComment}
       />
+
+      {/* 댓글 작성이 비어 있을 경우 모달 */}
+      {isModalOpen && (
+        <Modal>
+          <div className="text-center p-4">
+            <h2 className="flex justify-center">
+              <IoMdWarning className="text-yellow-500 text-4xl" />
+            </h2>
+            <p className="mt-2">{modalMessage}</p>
+            <button
+              onClick={handleCloseModal}
+              className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md"
+            >
+              닫기
+            </button>
+          </div>
+        </Modal>
+      )}
     </>
   );
 };
