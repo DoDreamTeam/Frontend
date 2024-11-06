3d04a3d9a98c14606100fc4cad18f0ff06179e35
codesche <codesche@gmail.com>
2024-11-06 17:19:20 +0900

    feat: 스터디 만들기 폼 (#57)

--- src/pages/CreateStudy.jsx
+++ src/pages/CreateStudy.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import CreateStudyForm from "../components/createStudy/CreateStudyForm";
 
 const CreateStudy = () => {
-  return <div>스터디 만들기 페이지</div>;
+  return (
+    <div className="w-full mb-16">
+      <div className="text-3xl text-blue-600 font-bold mb-16 text-left">
+        스터디 만들기
+      </div>
+
+      {/* 스터디 만들기 form */}
+      <CreateStudyForm />
+    </div>
+  );
 };
 
 export default CreateStudy;
