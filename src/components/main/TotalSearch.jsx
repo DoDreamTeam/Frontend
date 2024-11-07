7517948203c7b096a73d60ab9631dc2053f430c8
seoyeon-jung <jungluchia1204@gmail.com>
2024-11-07 11:49:57 +0900

    fix: loading 오류 수정

--- src/components/main/TotalSearch.jsx
+++ src/components/main/TotalSearch.jsx
@@ -46,9 +46,6 @@ const TotalSearch = () => {
           placeholder="검색어를 입력하세요"
         />
       </div>
-
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error: {error.message}</p>}
     </div>
   );
 };
